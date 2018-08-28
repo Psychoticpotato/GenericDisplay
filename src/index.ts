76a227f87cba4db9986e17b352dd9434b4b93c0f
Carroll <William@CarrollCloud.com>
2018-08-27 21:08:31 -0500

    Index to export members

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/* Grab everything to export */
+import * as asInterface from './general/dataDisplay'
+import * as asAbstract from './general/asAbstract'
+/* Now, export */
+export { asAbstract, asInterface }
