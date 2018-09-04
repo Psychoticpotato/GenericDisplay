d2acaa6ed5d42ae6ebf6f2bd0bd1ad87871406dd
Carroll <William@CarrollCloud.com>
2018-09-03 21:55:04 -0500

    GenerateTabs does just that
    It checks to see if annotation is present.  If not, it will 
    auto-generate (as best it can).

---
+++ src/display/generateTabs.tsx
@@ -0,0 +1,31 @@
+import * as DataDisplay from '../general/dataDisplay.js'
+import { newTab } from './elements.js'
+import { tabsFromObj } from '../general/annotationBlank.js'
+
+/**
+ * Generates a list of html tabs with the specified info.
+ * If 'DataDisplay' does not have annotation, a generic set will be generated
+ * @param  {[type]}  input           DataDisplay object
+ * @param  {Boolean} [devMode=false] PLANNED FEATURE: GUI to modify GUI
+ * @return {[type]}                  List of HTML tabs; BASIC CSS TO FOLLOW
+ */
+export function generateTabs(input: DataDisplay.DataDisplay /*, devMode = false */): HTMLElement[] {
+  /** Tab elements to return */
+  const tabs: HTMLElement[] = []
+
+  for (const key in input.annotation) {
+    if (input.annotation.hasOwnProperty(key)) {
+      // Generate the new tab
+      tabs.push(newTab(input.annotation[key], key))
+    }
+  }
+  // If we have an empty tab array, we have to generate it another way
+  if (tabs.length === 0) {
+    const objTab = tabsFromObj(input.data)
+    objTab.forEach((tab) => {
+      tabs.push(newTab(tab, tab.__name))
+    })
+  }
+
+  return tabs
+}
